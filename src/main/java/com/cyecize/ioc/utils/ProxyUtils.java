a8b04685ead809b486450266df40d1d2015d9033
Tsvetelin Yakimov <ceci2205@abv.bg>
2019-10-22 16:30:43 +0300

    Improments in ProxyUtils.

--- src/main/java/com/cyecize/ioc/utils/ProxyUtils.java
+++ src/main/java/com/cyecize/ioc/utils/ProxyUtils.java
@@ -9,37 +9,17 @@ import javassist.util.proxy.ProxyObject;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
 
 public class ProxyUtils {
 
     public static void createProxyInstance(ServiceDetails serviceDetails, Object[] constructorParams) {
         final ProxyFactory proxyFactory = new ProxyFactory();
         proxyFactory.setSuperclass(serviceDetails.getServiceType());
-        final Class<?> cls = proxyFactory.createClass();
 
         Object proxyInstance;
         try {
-
-            proxyInstance = Arrays.stream(cls.getDeclaredConstructors())
-                    .filter(ctr -> {
-                        if (ctr.getParameterCount() != constructorParams.length) {
-                            return false;
-                        }
-
-                        final Class<?>[] parameterTypes = ctr.getParameterTypes();
-
-                        for (int i = 0; i < parameterTypes.length; i++) {
-                            if (constructorParams[i] != null && !parameterTypes[i].isAssignableFrom(constructorParams[i].getClass())) {
-                                return false;
-                            }
-                        }
-
-                        ctr.setAccessible(true);
-                        return true;
-                    }).findFirst().get().newInstance(constructorParams);
-
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            proxyInstance = proxyFactory.create(serviceDetails.getTargetConstructor().getParameterTypes(), constructorParams);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
 
